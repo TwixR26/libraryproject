d94eaa254e9d152efdb793d34ed6501088669205
Rayan Alharbi <421109529@qu.edu.sa>
2026-04-10 09:19:52 +0300

    Done with Lab 6 - HTML Form and Handling

--- apps/usermodule/views.py
+++ apps/usermodule/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def __getBooksList():
+    book1 = {'id': 12344321, 'title': 'Continuous Delivery', 'author': 'J.Humble and D. Farley'}
+    book2 = {'id': 56788765, 'title': 'Reversing: Secrets of Reverse Engineering', 'author': 'E. Eilam'}
+    book3 = {'id': 43211234, 'title': 'The Hundred-Page Machine Learning Book', 'author': 'Andriy Burkov'}
+    return [book1, book2, book3]
+
